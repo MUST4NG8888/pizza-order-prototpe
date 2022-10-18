945ffe8af7d5a450cd76b82106532eaaa6c02e71
Drew <andrew.lendvai@gmail.com>
2022-10-18 14:18:50 +0200

    Basic backend Post and Get are working

--- backend/server.js
+++ backend/server.js
@@ -7,25 +7,25 @@ const port = 3000
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static('backend'))
+
 app.get('/api/pizza', (req, res) => {
 
     const searchParam = req.query.filter
 
-    const data = fileSystem.readFileSync("./database.json")
-    const recipes = JSON.parse(data)
+    const data = fileSystem.readFileSync(__dirname + '/api/pizza/pizza.json')
+    const pizzas = JSON.parse(data)
 
-    if (searchParam) {
-        return res.json(recipes.filter(recipe => recipe.title.includes(searchParam)))
-    }
-
-    res.json(recipes)
+    res.json(pizzas)
 })
 
 app.post('/api/orders', (req, res) => {
-  
-    const id = req.body.id
+    const data = fileSystem.readFileSync(__dirname + '/api/orders/orders.json')
+    const orders = JSON.parse(data)
+    const lastOrder = orders[orders.length-1]
+      
     const orderDate = req.body.orderDate
-    const pizza = req.body.pizza
+    const orderedItems = req.body.orderedItems
     const name = req.body.name
     const street = req.body.street
     const houseNumber = req.body.houseNumber
@@ -33,28 +33,23 @@ app.post('/api/orders', (req, res) => {
     const zipCode = req.body.zipCode
     const mobile = req.body.mobile
     const email = req.body.email
-  
-    const data = fileSystem.readFileSync(__dirname + "./orders.json")
-    const orders = JSON.parse(data)
-    const lastOrder = orders[orders.length-1]
 
     const newOrders = {
         "id" : Object.values(lastOrder)[0] + 1,
-        "orderDate": "2022-10-17",
-        "pizza": {"id" : 1, "name": "Margarita", "amount": 3},
-        "name": "Béla",
-        "street": "Bajcsy",
-        "houseNumber": "8",
-        "city": "Budapest",
-        "zipCode": "1057",
-        "mobile": "+36307777777",
-        "email": "pizzaforever2@gmail.com"
+        "orderDate": orderDate,
+        "orderedItems": orderedItems,
+        "name": name,
+        "street": street,
+        "houseNumber": houseNumber,
+        "city": city,
+        "zipCode": zipCode,
+        "mobile": mobile,
+        "email": email
       }
 
-  
     orders.push(newOrders)
     const newData = JSON.stringify(orders)
-    fileSystem.writeFileSync(__dirname + "./orders.json", newData)
+    fileSystem.writeFileSync(__dirname + '/api/orders/orders.json', newData)
 
     res.sendStatus(204)
 })
