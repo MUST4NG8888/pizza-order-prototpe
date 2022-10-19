efba079214dcea40a20f6b6bbf37877729265237
Drew <andrew.lendvai@gmail.com>
2022-10-19 14:39:28 +0200

    Backend orders separated into jsons

--- backend/server.js
+++ backend/server.js
@@ -1,59 +1,67 @@
-const express = require('express')
-const fileSystem = require('fs')
-const cors = require("cors")
-const app = express()
-const port = 3000
+const express = require("express");
+const fileSystem = require("fs");
+const cors = require("cors");
+const app = express();
+const port = 3000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
+app.use("/images", express.static(__dirname + "/api/pizza/images"));
 
-app.use("/images", express.static(__dirname + '/api/pizza/images'));
+app.get("/api/pizza", (req, res) => {
+  const data = fileSystem.readFileSync(__dirname + "/api/pizza/pizza.json");
+  const pizzas = JSON.parse(data);
 
-app.get('/api/pizza', (req, res) => {
+  res.json(pizzas);
+});
 
+app.post("/api/orders", (req, res) => {
+  const data = fileSystem.readFileSync(__dirname + "/api/orders/orders1.json");
+  const orders = JSON.parse(data);
+  const files = fileSystem.readdirSync(__dirname + "/api/orders/");
+  console.log(files);
 
-    const data = fileSystem.readFileSync(__dirname + '/api/pizza/pizza.json')
-    const pizzas = JSON.parse(data)
+  const currentOrder = [];
+  // const lastOrder = orders[orders.length-1]
+  const lastOrder = files[files.length - 1];
+  const r = /\d+/g;
+  const lastOrderNumberStr = lastOrder.match(r);
+  const lastOrderNumber = Number(lastOrderNumberStr[0]);
 
-    res.json(pizzas)
-})
+  const orderDate = req.body.orderDate;
+  const orderedItems = req.body.pizza;
+  const name = req.body.name;
+  const street = req.body.street;
+  const houseNumber = req.body.houseNumber;
+  const city = req.body.city;
+  const zipCode = req.body.zipCode;
+  const mobile = req.body.mobile;
+  const email = req.body.email;
 
-app.post('/api/orders', (req, res) => {
-    const data = fileSystem.readFileSync(__dirname + '/api/orders/orders.json')
-    const orders = JSON.parse(data)
-    const lastOrder = orders[orders.length-1]
-      
-    const orderDate = req.body.orderDate
-    const orderedItems = req.body.pizza
-    const name = req.body.name
-    const street = req.body.street
-    const houseNumber = req.body.houseNumber
-    const city = req.body.city
-    const zipCode = req.body.zipCode
-    const mobile = req.body.mobile
-    const email = req.body.email
+  const newOrders = {
+    id: lastOrderNumber + 1,
+    orderDate: orderDate,
+    orderedItems: orderedItems,
+    name: name,
+    street: street,
+    houseNumber: houseNumber,
+    city: city,
+    zipCode: zipCode,
+    mobile: mobile,
+    email: email,
+  };
 
-    const newOrders = {
-        "id" : Object.values(lastOrder)[0] + 1,
-        "orderDate": orderDate,
-        "orderedItems": orderedItems,
-        "name": name,
-        "street": street,
-        "houseNumber": houseNumber,
-        "city": city,
-        "zipCode": zipCode,
-        "mobile": mobile,
-        "email": email
-      }
+  currentOrder.push(newOrders);
+  const newData = JSON.stringify(currentOrder);
+  fileSystem.writeFileSync(
+    __dirname + `/api/orders/orders${lastOrderNumber + 1}.json`,
+    newData
+  );
 
-    orders.push(newOrders)
-    const newData = JSON.stringify(orders)
-    fileSystem.writeFileSync(__dirname + '/api/orders/orders.json', newData)
-
-    res.sendStatus(204)
-})
+  res.sendStatus(204);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
