465c51f6a8a36ac1c6b344d32958eb34ceeaf3ff
Drew <andrew.lendvai@gmail.com>
2022-10-20 02:06:23 +0200

    CSS added,fixed, halfcircle button added

--- frontend/script.js
+++ frontend/script.js
@@ -1,154 +1,229 @@
-const nameInput = document.getElementById("name")
-const streetInput = document.getElementById("street")
-const houseNumberInput = document.getElementById("houseNumber")
-const cityInput = document.getElementById("city")
-const zipCodeInput = document.getElementById("zipCode")
-const mobileInput = document.getElementById("mobile")
-const emailInput = document.getElementById("email")
+const nameInput = document.getElementById("name");
+const streetInput = document.getElementById("street");
+const houseNumberInput = document.getElementById("houseNumber");
+const cityInput = document.getElementById("city");
+const zipCodeInput = document.getElementById("zipCode");
+const mobileInput = document.getElementById("mobile");
+const emailInput = document.getElementById("email");
 
 
-let request = async () => {
-    let response = await fetch(`http://localhost:3000/api/pizza`)
-    let pizza = await response.json()
+const appState = {
+  pizza: [],
+};
+const createButtons = () => {
+  for (let i = 0; i < appState.pizza.length; i++) {
+    let pizzaButton = document.createElement("button");
+    pizzaButtonDiv.append(pizzaButton);
+    pizzaButton.className = `pizzabutton`;
+    pizzaButton.id = `circle${i}`;
+  }
+};
 
+const pizzaLoader = (a) => {
+  let pizzaContainer = document.getElementById("pizzaContainer");
+  pizzaContainer.innerHTML = "";
+  let pizzaBox = document.createElement("div");
+  pizzaBox.className = "pizzabox";
+  pizzaContainer.append(pizzaBox);
+  let pizzaLeft = document.createElement("div");
+  pizzaLeft.id = "pizza_left";
+  let pizzaRight = document.createElement("div");
+  pizzaRight.id = "pizza_right";
+  pizzaBox.append(pizzaLeft);
+  pizzaBox.append(pizzaRight);
+  let pizzaButtonDiv = document.createElement("div");
+  document.getElementById("pizza_right").append(pizzaButtonDiv);
+  pizzaButtonDiv.id = "pizzaButtonDiv"
 
-    const pizzaLoader = () => {
-        let pizzaContainer = document.getElementById("pizzaContainer");
-        for(let i = 0; i < pizza.length; i++){
+  let pizzaImg = document.createElement("img");
+  pizzaImg.className = "pizza_img";
+  console.log(appState);
+  pizzaImg.src = `http://127.0.0.1:3000${appState.pizza[a].picture}`;
 
-            
-            let pizzaBox = document.createElement("div")
-            pizzaBox.className = "pizzabox" 
-            pizzaContainer.append(pizzaBox); 
-            let pizzaLeft = document.createElement("div")
-            pizzaLeft.id = "pizza_left"
-            let pizzaRight = document.createElement("div")
-            pizzaRight.id = "pizza_right"
-            pizzaBox.append(pizzaLeft)
-            pizzaBox.append(pizzaRight)
+  pizzaRight.append(pizzaImg);
 
-            let pizzaImg = document.createElement("img")
-            pizzaImg.className = "pizza_img" ;
+  let pizzaName = document.createElement("h1");
+  pizzaLeft.append(pizzaName);
+  pizzaName.innerText = appState.pizza[a].name;
+  let pizzaToppings = document.createElement("h3");
+  pizzaLeft.append(pizzaToppings);
+  pizzaToppings.innerText = appState.pizza[a].toppings;
+  const amountInput = document.createElement("input");
+  amountInput.type = "number";
+  amountInput.placeholder = "Amount";
+  pizzaLeft.append(amountInput);
+  const mainButton = document.createElement("button");
+  pizzaLeft.append(mainButton);
+  mainButton.value = 0;
+  mainButton.innerText = "Add to Basket";
+};
 
-            // let pizzaPath = path.join(__dirname, '..', '', 'pizza.js')
+const request = async () => {
+  const response = await fetch(`http://localhost:3000/api/pizza`);
+  const data = await response.json();
+  appState.pizza = data;
+  pizzaLoader(1);
+  createButtons();
+};
 
-            pizzaImg.src = `http://127.0.0.1:3000${pizza[i].picture}`
-            console.log(pizza[i].picture)
-            
-            pizzaRight.append(pizzaImg);
+request();
 
-            let pizzaName = document.createElement("h1")
-            pizzaLeft.append(pizzaName);
-            pizzaName.innerText = pizza[i].name
-            let pizzaToppings = document.createElement("h3")
-            pizzaLeft.append(pizzaToppings) 
-            pizzaToppings.innerText = pizza[i].toppings
-            
-            console.log("cica");
-        }
-    }
-    pizzaLoader()
-}
-
-
-
-const postOrder = async (date, pizza, name, street, houseNumber, city, zipcode, mobile, email) => {
-    const url = "http://localhost:3000/api/orders"
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({date, pizza, name, street, houseNumber, city, zipcode, mobile, email})
-    })
-    return response.status
-}
-
-
-
+const postOrder = async (
+  date,
+  pizza,
+  name,
+  street,
+  houseNumber,
+  city,
+  zipcode,
+  mobile,
+  email
+) => {
+  const url = "http://localhost:3000/api/orders";
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      date,
+      pizza,
+      name,
+      street,
+      houseNumber,
+      city,
+      zipcode,
+      mobile,
+      email,
+    }),
+  });
+  return response.status;
+};
 
 const resetForm = () => {
-    nameInput.value = ""
-    streetInput.value = ""
-    houseNumberInput.value = null
-    cityInput.value = ""
-    zipCodeInput.value = null
-    mobileInput.value = null
-    emailInput.value = ""
-}
+  nameInput.value = "";
+  streetInput.value = "";
+  houseNumberInput.value = null;
+  cityInput.value = "";
+  zipCodeInput.value = null;
+  mobileInput.value = null;
+  emailInput.value = "";
+};
 
-
-const makeOrder = async (date, pizza, name, street, houseNumber, city, zipcode, mobile, email) => {
-    const resStatus = await postOrder(date, pizza, name, street, houseNumber, city, zipcode, mobile, email)
-    if (resStatus === 204) {
-        alert("Thanks for your love of Pizza!")
-        resetForm()
-        console.log("kettes szint");
-    } else {
-        alert("Something goes wrong")
-    }
-}
+const makeOrder = async (
+  date,
+  pizza,
+  name,
+  street,
+  houseNumber,
+  city,
+  zipcode,
+  mobile,
+  email
+) => {
+  const resStatus = await postOrder(
+    date,
+    pizza,
+    name,
+    street,
+    houseNumber,
+    city,
+    zipcode,
+    mobile,
+    email
+  );
+  if (resStatus === 204) {
+    alert("Thanks for your love of Pizza!");
+    resetForm();
+  } else {
+    alert("Something goes wrong");
+  }
+};
 
 const getOrderInput = async () => {
-    let orderName = nameInput.value;
+  let orderName = nameInput.value;
 
-    let chosenPizza = [{
-        'id' : 4,
-        'name' : "Margareta",
-        'amount' : 2
-    }, {
-        'id' : 5,
-        'name' : "Margareta",
-        'amount' : 24
-    }, {
-        'id' : 64,
-        'name' : "Margareta",
-        'amount' : 23
-    }]
-    
-    let orderStreet = streetInput.value;
-    let orderHouseNumber = houseNumberInput.value;
-    let orderCity = cityInput.value;
-    let orderZipCode = zipCodeInput.value;
-    let orderMobile = mobileInput.value;
-    let orderEmail = emailInput.value;
+  let chosenPizza = [
+    {
+      id: 4,
+      name: "Margareta",
+      amount: 2,
+    },
+    {
+      id: 5,
+      name: "Margareta",
+      amount: 24,
+    },
+    {
+      id: 64,
+      name: "Margareta",
+      amount: 23,
+    },
+  ];
 
-    
-    let Today = new Date();
-    var date = Today.getFullYear()+'-'+(Today.getMonth()+1)+'-'+Today.getDate() + ' ' + Today.getHours() + ':' + Today.getMinutes() + ":" + Today.getSeconds();
-    console.log(date)
-    
-    console.log("egyes szint");
-    await makeOrder(date, chosenPizza , orderName, orderStreet, orderHouseNumber, orderCity, orderZipCode, orderMobile, orderEmail);
-}
+  let orderStreet = streetInput.value;
+  let orderHouseNumber = houseNumberInput.value;
+  let orderCity = cityInput.value;
+  let orderZipCode = zipCodeInput.value;
+  let orderMobile = mobileInput.value;
+  let orderEmail = emailInput.value;
 
-request ();
+  let Today = new Date();
+  var date =
+    Today.getFullYear() +
+    "-" +
+    (Today.getMonth() + 1) +
+    "-" +
+    Today.getDate() +
+    " " +
+    Today.getHours() +
+    ":" +
+    Today.getMinutes() +
+    ":" +
+    Today.getSeconds();
 
+  await makeOrder(
+    date,
+    chosenPizza,
+    orderName,
+    orderStreet,
+    orderHouseNumber,
+    orderCity,
+    orderZipCode,
+    orderMobile,
+    orderEmail
+  );
+};
 
-
-
-
-    //addButton.addEventListener("click", createRecipe)
-    //document.getElementById("random").addEventListener("click", () => console.log("random"))
-    
-
-const ourButton = document.getElementById("orderButton")
+const ourButton = document.getElementById("orderButton");
 ourButton.addEventListener("click", getOrderInput);
 
-
-   
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+  document.getElementById("myNav").style.width = "100%";
+}
 
-  function openBusket() {
-    document.getElementById("myBusket").style.width = "100%";
-  }
-  
-  function closeBusket() {
-    document.getElementById("myBusket").style.width = "0%";
-  }
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+function openBasket() {
+  document.getElementById("myBasket").style.width = "100%";
+}
+
+function closeBasket() {
+  document.getElementById("myBasket").style.width = "0%";
+}
+
+document.getElementById("hamburger_menu").addEventListener("click", openNav);
+document.getElementById("basket").addEventListener("click", openBasket);
+document
+  .getElementById("closebtnHamburger")
+  .addEventListener("click", closeNav);
+document
+  .getElementById("closebtnBasket")
+  .addEventListener("click", closeBasket);
+
+// document.getElementById('0').addEventListener("click", function (e) {
+//   a = e.target.value
+//   pizzaLoader(a);
+// });
